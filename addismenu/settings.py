384315b0fcf4124476ddc798a96abde7ecd59345
yabtsega <yabtsega853@gmail.com>
2026-06-11 02:05:18 -0900

    Fix database to use DATABASE_URL

--- addismenu/settings.py
+++ addismenu/settings.py
@@ -1,7 +1,8 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 import dj_database_url
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default--key-for-dev')
