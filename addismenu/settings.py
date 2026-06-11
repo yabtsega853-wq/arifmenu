3092fcb81d2d4e004530926530537254c46770ad
yb <yabtsega853@gmail.com>
2026-06-11 01:50:19 -0900

    Update settings.py

--- addismenu/settings.py
+++ addismenu/settings.py
@@ -91,3 +91,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = '/'   # Send user to home page after login
+STATIC_ROOT = BASE_DIR / 'staticfiles'
